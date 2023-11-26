71ac6234f1c74c2f0fa29773a505c9d99bd15e03
JosaStyleNevi <143549191+JosaStyleNevi@users.noreply.github.com>
2023-11-26 15:35:42 +0000

    Act_exp

--- Hello.py
+++ Hello.py
@@ -1,4 +1,5 @@
 import streamlit as st
 import pandas as pd
-excel=pd.read_excel("Copia de REPORTE COMENSALES - IMCO 23.09.23.xlsx")
-st.data_editor(excel)
+xls= pd.ExcelFile(f"https://docs.google.com/spreadsheets/d/1ib2USv--f8fDn9t1Mhtp_FWOCWZfp9xV/export?format=xlsx")
+xls = pd.read_excel(xls)
+st.data_editor(xls)
