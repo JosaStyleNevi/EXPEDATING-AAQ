e0ae1a272782c8d0cb1407fee46fcfcf7b91f2f9
JosaStyleNevi <143549191+JosaStyleNevi@users.noreply.github.com>
2023-11-27 02:24:01 +0000

    Final_1

--- Hello.py
+++ Hello.py
@@ -1,5 +1,16 @@
 import streamlit as st
-import pandas as pd
-xls= pd.ExcelFile(f"https://docs.google.com/spreadsheets/d/1ib2USv--f8fDn9t1Mhtp_FWOCWZfp9xV/export?format=xlsx")
-xls = pd.read_excel(xls)
-st.data_editor(xls)
+
+# Configurar el título de la aplicación
+st.title("Menú Lateral con Streamlit")
+
+# Botones en lugar de cuadro desplegable
+if st.sidebar.button("Expedating de Compras"):
+    st.write("Bienvenido a la página de inicio.")
+
+if st.sidebar.button("Materiales para Anddes"):
+    st.write("Configura tus preferencias aquí.")
+
+if st.sidebar.button("Materiales Surplus"):
+    st.write("¿Necesitas ayuda? ¡Estamos aquí para ayudarte!")
+
+# Puedes agregar más opciones y contenido según tus necesidades
